ebad7cc50877f7360c61d4719d04ea777fe25949
markel1974 <markel@tin.it>
2026-03-16 19:24:59 +0100

    update

--- mr_tech/main.go
+++ mr_tech/main.go
@@ -32,7 +32,7 @@ func main() {
 	case 1:
 		cfg, err = world.Generate()
 	case 2:
-		const levelNumber = 3
+		const levelNumber = 1
 		wadFile := "resources" + string(os.PathSeparator) + "wad" + string(os.PathSeparator) + "DOOM.WAD"
 		wb := wad.NewBuilder()
 		cfg, err = wb.Setup(wadFile, levelNumber)

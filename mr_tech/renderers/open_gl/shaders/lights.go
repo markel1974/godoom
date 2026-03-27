fa20268ab0f80d94c94e3f545d3e545571f998c5
markel1974 <markel@tin.it>
2026-03-27 07:54:44 +0100

    update

--- mr_tech/renderers/open_gl/shaders/lights.go
+++ mr_tech/renderers/open_gl/shaders/lights.go
@@ -9,22 +9,27 @@ import (
 type LightLoc int
 
 const (
-	LightLocGAlbedoSpec = LightLoc(iota)
-	LightLocGNormalEmiss
-	LightLocGPositionDepth
+	LightLocProjection = LightLoc(iota)
 	LightLocView
 	LightLocInvView
+	LightLocRoomSpaceMatrix
+	LightLocTexture
+	LightLocNormalMap
+	LightLocRoomShadowMap
+	LightLocScreenResolution
 	LightLocAmbientLight
+	LightLocEnableShadows
+	LightLocVolumetricSteps
+	LightLocBeamRatioFactor
 	LightLocLast
 )
 
 type Lights struct {
 	prg   uint32
 	table [LightLocLast]int32
-	ubo   uint32
 }
 
-func NewShaderLight() *Lights {
+func NewLights() *Lights {
 	return &Lights{}
 }
 
@@ -32,9 +37,9 @@ func (s *Lights) Setup(width, height int32) {}
 
 func (s *Lights) SetupSamplers() {
 	gl.UseProgram(s.prg)
-	gl.Uniform1i(s.GetUniform(LightLocGAlbedoSpec), 0)
-	gl.Uniform1i(s.GetUniform(LightLocGNormalEmiss), 1)
-	gl.Uniform1i(s.GetUniform(LightLocGPositionDepth), 2)
+	gl.Uniform1i(s.GetUniform(LightLocTexture), 0)
+	gl.Uniform1i(s.GetUniform(LightLocNormalMap), 1)
+	gl.Uniform1i(s.GetUniform(LightLocRoomShadowMap), 3)
 }
 
 func (s *Lights) GetUniform(id LightLoc) int32 {
@@ -42,7 +47,7 @@ func (s *Lights) GetUniform(id LightLoc) int32 {
 }
 
 func (s *Lights) Compile(a IAssets) error {
-	vSrc, fSrc, err := a.ReadMulti("post.vert", "light.frag") // Usa il vertex shader del full-screen quad
+	vSrc, fSrc, err := a.ReadMulti("main.vert", "lights.frag")
 	if err != nil {
 		return err
 	}
@@ -62,47 +67,46 @@ func (s *Lights) Compile(a IAssets) error {
 		return err
 	}
 
-	s.table[LightLocGAlbedoSpec] = gl.GetUniformLocation(s.prg, gl.Str("gAlbedoSpec\x00"))
-	s.table[LightLocGNormalEmiss] = gl.GetUniformLocation(s.prg, gl.Str("gNormalEmiss\x00"))
-	s.table[LightLocGPositionDepth] = gl.GetUniformLocation(s.prg, gl.Str("gPositionDepth\x00"))
+	s.table[LightLocProjection] = gl.GetUniformLocation(s.prg, gl.Str("u_projection\x00"))
 	s.table[LightLocView] = gl.GetUniformLocation(s.prg, gl.Str("u_view\x00"))
 	s.table[LightLocInvView] = gl.GetUniformLocation(s.prg, gl.Str("u_invView\x00"))
+	s.table[LightLocRoomSpaceMatrix] = gl.GetUniformLocation(s.prg, gl.Str("u_roomSpaceMatrix\x00"))
+	s.table[LightLocTexture] = gl.GetUniformLocation(s.prg, gl.Str("u_texture\x00"))
+	s.table[LightLocNormalMap] = gl.GetUniformLocation(s.prg, gl.Str("u_normalMap\x00"))
+	s.table[LightLocRoomShadowMap] = gl.GetUniformLocation(s.prg, gl.Str("u_roomShadowMap\x00"))
+	s.table[LightLocScreenResolution] = gl.GetUniformLocation(s.prg, gl.Str("u_screenResolution\x00"))
 	s.table[LightLocAmbientLight] = gl.GetUniformLocation(s.prg, gl.Str("u_ambient_light\x00"))
+	s.table[LightLocEnableShadows] = gl.GetUniformLocation(s.prg, gl.Str("u_enableShadows\x00"))
+	s.table[LightLocVolumetricSteps] = gl.GetUniformLocation(s.prg, gl.Str("u_volumetricSteps\x00"))
+	s.table[LightLocBeamRatioFactor] = gl.GetUniformLocation(s.prg, gl.Str("u_beamRatioFactor\x00"))
 
 	for idx, v := range s.table {
 		if v < 0 {
-			return fmt.Errorf("invalid uniform location in lights: %d", idx)
+			return fmt.Errorf("invalid uniform location in lights: %d\n", idx)
 		}
 	}
 
-	// Setup UBO Binding
 	blockIndex := gl.GetUniformBlockIndex(s.prg, gl.Str("LightsBlock\x00"))
 	if blockIndex != gl.INVALID_INDEX {
-		gl.UniformBlockBinding(s.prg, blockIndex, 0) // Bind al point 0
+		gl.UniformBlockBinding(s.prg, blockIndex, 0)
 	}
 
 	return nil
 }
 
-func (s *Lights) Render(drawScreenQuad func(), view, invView [16]float32, ambient float32, uboLights uint32) {
+func (s *Lights) Render(proj, view, invView, roomSpace [16]float32, ambient, spotIntensity, beamRatio float32, enableShadows int32, volSteps int32, screenW, screenH float32, uboLights uint32) {
 	gl.UseProgram(s.prg)
 
-	// Additive blending per accumulo radianza
-	gl.Enable(gl.BLEND)
-	gl.BlendFunc(gl.ONE, gl.ONE)
-	gl.DepthMask(false)
-	gl.Disable(gl.DEPTH_TEST)
-
+	gl.UniformMatrix4fv(s.GetUniform(LightLocProjection), 1, false, &proj[0])
 	gl.UniformMatrix4fv(s.GetUniform(LightLocView), 1, false, &view[0])
 	gl.UniformMatrix4fv(s.GetUniform(LightLocInvView), 1, false, &invView[0])
+	gl.UniformMatrix4fv(s.GetUniform(LightLocRoomSpaceMatrix), 1, false, &roomSpace[0])
+
+	gl.Uniform2f(s.GetUniform(LightLocScreenResolution), screenW, screenH)
 	gl.Uniform1f(s.GetUniform(LightLocAmbientLight), ambient)
+	gl.Uniform1i(s.GetUniform(LightLocEnableShadows), enableShadows)
+	gl.Uniform1i(s.GetUniform(LightLocVolumetricSteps), volSteps)
+	gl.Uniform1f(s.GetUniform(LightLocBeamRatioFactor), beamRatio)
 
-	// Bind dell'UBO popolato dal BatchBuilder
 	gl.BindBufferBase(gl.UNIFORM_BUFFER, 0, uboLights)
-
-	drawScreenQuad()
-
-	gl.Disable(gl.BLEND)
-	gl.DepthMask(true)
-	gl.Enable(gl.DEPTH_TEST)
 }

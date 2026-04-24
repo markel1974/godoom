ddb3878ddc0e2aa7ac1e3bb63ecedcc0ee779a98
markel1974 <markel@tin.it>
2026-04-24 05:44:43 +0200

    update

--- mr_tech/model/compiler.go
+++ mr_tech/model/compiler.go
@@ -84,7 +84,7 @@ func (r *Compiler) Compile(cfg *config.Root) error {
 	r.volumes.Setup()
 
 	r.lights.AddLights(r.compileLights(cfg.Lights))
-	r.things = NewThings(r.gScale, 10, cfg.Things, r.volumes, animations)
+	r.things = NewThings(full3d, r.gScale, 10, cfg.Things, r.volumes, animations)
 	r.player = NewThingPlayer(r.things, cfg.Player, r.volumes, false)
 	if r.player == nil {
 		return fmt.Errorf("player not found")

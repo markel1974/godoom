48e85145777e946e5ea8ac1282d03702bffbfc16
markel1974 <markel@tin.it>
2026-09-23 15:04:47 +0200

    update

--- mr_tech/renderers/open_gl/builder_volume.go
+++ mr_tech/renderers/open_gl/builder_volume.go
@@ -296,28 +296,43 @@ func (w *BuilderVolume) pushQThings(things *model.Things, frustumFront *physics.
 		tPosX, tPosY, zBot := thing.GetDisplacement()
 		oX, oY, oZ := float32(tPosX), float32(zBot), float32(-tPosY)
 		b := float32(billBoard)
-		startIndices := w.fv.GetIndicesLen()
-		for fx := 0; fx < faceCount; fx++ {
-			f := (*faces2)[fx]
-			mat := f.GetMaterial()
-			if mat == nil {
-				continue
+		pushPassThing := func(targetBlendMode int, targetDc *DrawCommands) {
+			startIndices := w.fv.GetIndicesLen()
+			var added int
+			for fx := 0; fx < faceCount; fx++ {
+				f := (*faces2)[fx]
+				mat := f.GetMaterial()
+				if mat == nil {
+					continue
+				}
+				blendMode := config.BlendModeOpaque
+				if mat.IsEmissive() {
+					blendMode = config.BlendModeAdditive
+				}
+				if blendMode != targetBlendMode {
+					continue
+				}
+				l, ok := w.tex.Get(mat)
+				if !ok {
+					continue
+				}
+				p := f.GetPoints()
+				u, v := f.GetUV()
+				np := (*nextFaces2)[fx].GetPoints()
+				id0 := w.fv.AddVertex15(float32(p[0].X), float32(p[0].Z), float32(-p[0].Y), float32(u[0]), float32(-v[0]), l, oX, oY, oZ, b, float32(np[0].X), float32(np[0].Z), float32(-np[0].Y), lerp, yaw)
+				id1 := w.fv.AddVertex15(float32(p[1].X), float32(p[1].Z), float32(-p[1].Y), float32(u[1]), float32(-v[1]), l, oX, oY, oZ, b, float32(np[1].X), float32(np[1].Z), float32(-np[1].Y), lerp, yaw)
+				id2 := w.fv.AddVertex15(float32(p[2].X), float32(p[2].Z), float32(-p[2].Y), float32(u[2]), float32(-v[2]), l, oX, oY, oZ, b, float32(np[2].X), float32(np[2].Z), float32(-np[2].Y), lerp, yaw)
+				w.fv.AddTriangle(id0, id1, id2)
+				added++
 			}
-			l, ok := w.tex.Get(mat)
-			if !ok {
-				continue
+			currentIndices := w.fv.GetIndicesLen()
+			if added > 0 && startIndices != currentIndices {
+				targetDc.Compute(startIndices, currentIndices)
 			}
-			p := f.GetPoints()
-			u, v := f.GetUV()
-			np := (*nextFaces2)[fx].GetPoints()
-			id0 := w.fv.AddVertex15(float32(p[0].X), float32(p[0].Z), float32(-p[0].Y), float32(u[0]), float32(-v[0]), l, oX, oY, oZ, b, float32(np[0].X), float32(np[0].Z), float32(-np[0].Y), lerp, yaw)
-			id1 := w.fv.AddVertex15(float32(p[1].X), float32(p[1].Z), float32(-p[1].Y), float32(u[1]), float32(-v[1]), l, oX, oY, oZ, b, float32(np[1].X), float32(np[1].Z), float32(-np[1].Y), lerp, yaw)
-			id2 := w.fv.AddVertex15(float32(p[2].X), float32(p[2].Z), float32(-p[2].Y), float32(u[2]), float32(-v[2]), l, oX, oY, oZ, b, float32(np[2].X), float32(np[2].Z), float32(-np[2].Y), lerp, yaw)
-			w.fv.AddTriangle(id0, id1, id2)
 		}
-		currentIndices := w.fv.GetIndicesLen()
-		w.dc.Compute(startIndices, currentIndices)
-		w.dcAdditive.Compute(startIndices, currentIndices)
+
+		pushPassThing(int(config.BlendModeOpaque), w.dc)
+		pushPassThing(int(config.BlendModeAdditive), w.dcAdditive)
 		counter++
 		return false
 	}
